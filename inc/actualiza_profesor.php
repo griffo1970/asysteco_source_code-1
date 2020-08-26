a0f8e15c62899ee64cc01a86a065e428b9c4bb58
asysteco <admin@asysteco.com>
2020-08-26 17:47:39 +0200

    HotFix conex

--- inc/actualiza_profesor.php
+++ inc/actualiza_profesor.php
@@ -3,8 +3,9 @@
 if($_POST['ID'] != '')
 {
     $sql = "UPDATE $class->profesores 
-    SET $class->profesores.Iniciales='$_POST[Iniciales]', $class->profesores.Nombre='$_POST[Nombre]', 
-    $class->profesores.Tutor='" . mysqli_real_escape_string($class->bdConex(), $_POST['Tutor']) . "' WHERE $class->profesores.ID=" . mysqli_real_escape_string($class->bdConex(), $_POST['ID']);
+    SET $class->profesores.Iniciales='$_POST[Iniciales]',
+    $class->profesores.Nombre='$_POST[Nombre]', 
+    $class->profesores.Tutor='" . mysqli_real_escape_string($class->conex, $_POST['Tutor']) . "' WHERE $class->profesores.ID=" . mysqli_real_escape_string($class->conex, $_POST['ID']);
     if($class->query($sql))
     {
         $MSG = "Datos actualizados correctamente.";
--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,29 +1,3 @@
 <div class="container" style="margin-top: 75px;"></div>
 <?php
 
-$basedir = dirname($_SERVER['DOCUMENT_ROOT']);
-$Instituto = preg_split('/\//', $_SERVER['REQUEST_URI']);
-$subdir = '/' . $Instituto[1];
-preg_match('/^\/[A-Z]+$/i', $subdir) ? $subdir = $subdir : $subdir = '' ;
-$dirs = [
-    'public' => '',
-    'inc' => $basedir . $subdir . '/inc/',
-    'class' => $basedir . $subdir . '/class/',
-];
-// try
-// {
-//     $bd = $class->bdConex();
-//     $bd->begin_transaction();
-//     $count = 6;
-//     for($i=0;$i<$count;$i++)
-//     {
-//         $i == 3 ? $columna = 'Iniciasles' : $columna = 'Iniciales';
-//         $response = $class->query("INSERT INTO Profesores ($columna, Nombre, Password, TIPO) VALUES ('FGL', 'Francisco García Lugo$i', 'luajhkgsfdkuqw7tei8972g3odcuiydo', '2')");
-//     }
-//     $bd->commit();
-//     $bd->autocommit(TRUE);
-// }
-// catch(Exception $ERR_ASYSTECO)
-// {
-//     $bd->rollback();
-// }
