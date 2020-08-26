50227d2e0cf9be406256ed42b53b69f4c908efd1
varo <varo@pop-os.localdomain>
2020-08-26 16:11:02 +0200

    import

--- inc/import-profesorado.php
+++ inc/import-profesorado.php
@@ -15,17 +15,23 @@
         </div>
     </form>
 <?php
-    if(! $num_profesores_act = $class->query("SELECT count(DISTINCT ID) as activos FROM $class->profesores WHERE Activo=1"))
+    if($num_profesores_act = $class->query("SELECT count(DISTINCT ID) as activos FROM $class->profesores WHERE Activo=1"))
+    {
+        $num_act = $num_profesores_act->fetch_assoc();
+    }
+    else
     {
         $ERR_MSG = $class->ERR_NETASYS;
     }
-    $num_act = $num_profesores_act->fetch_assoc();
     
-    if(! $num_profesores_all = $class->query("SELECT count(ID) as total FROM $class->profesores"))
+    if($num_profesores_all = $class->query("SELECT count(ID) as total FROM $class->profesores"))
+    {
+        $num_all = $num_profesores_all->fetch_assoc();
+    }
+    else
     {
         $ERR_MSG = $class->ERR_NETASYS;
     }
-    $num_all = $num_profesores_all->fetch_assoc();
     echo "<h3>Profesores totales: $num_all[total]</h3>";
     echo "<h3>Profesores activos: $num_act[activos]</h3>";
     echo "<a id='btn-todos-registros-prof' class='btn btn-info'>Ver todos los registros</a>";
