3438a87d2f6c184e069f7e25b8bceec6f00f23fb
asysteco <admin@asysteco.com>
2020-08-28 13:59:49 +0200

    qr generator

--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,3 +1,8 @@
 <div class="container" style="margin-top: 75px;"></div>
 <?php
-
+echo "<textarea id='data' onclick='create()'>Alfonso;as,jhdgfikwy9283gi2yd3g923d23</textarea>";
+echo "<div id='qrimage'></div>";
+echo "<script>";
+include_once($dirs['public'] . "js/qrgenerator.js");
+echo "</script>";
+echo ""
