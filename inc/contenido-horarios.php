be61ded59bf39d6123770db6f70b8682f9d3feed
asysteco <admin@asysteco.com>
2020-08-28 20:29:11 +0200

    mensajes con señalización en badges

--- inc/contenido-horarios.php
+++ inc/contenido-horarios.php
@@ -2,12 +2,12 @@
 <?php
 
 
-if(isset($_GET['ACTION']) && $_GET['ACTION'] == 'modificar-horario')
+if(isset($_GET['ACTION']) && $_GET['OPT'] == 'modificar-horario')
 {
     include_once($dirs['inc'] . 'horarios-edit.php');
     include_once "js/update_horario.js";
 }
-elseif(isset($_GET['ACTION']) && $_GET['ACTION'] == 'crear-horario')
+elseif(isset($_GET['ACTION']) && $_GET['OPT'] == 'crear-horario')
 {
     include_once($dirs['inc'] . 'crear-horario.php');
 }
