a5ed72a448eb1aac9aa1d9073972ff5e38add420
asysteco <70266663+asysteco@users.noreply.github.com>
2020-08-31 13:15:04 +0200

    griffo

--- inc/pruebas.php
+++ inc/pruebas.php
@@ -1,44 +1,49 @@
 <?php
 if($_SESSION['Perfil'] === 'Admin')
 { 
- if ($response = $class->query("SELECT $class->profesores.ID, $class->profesores.Nombre, $class->profesores.Iniciales, $class->profesores.Activo FROM $class->profesores WHERE $class->profesores[ID]='$_GET[ID]'")
- {
-   if ($response->num_rows > 0)
-   {
-    echo "<div id='guardias'></div>";
-    echo "<h2>Edición de Guardias</h2>";
-    echo "</br><table id='tabla_profesores' class='table table-hover'>";
-    echo "<thead>";
-        echo "<tr>";
-            echo "<th>ID</th>";
-            echo "<th>Nombre</th>";
-            echo "<th>Iniciales</th>";
-            echo "<th>Activo</th>";
-        echo "</tr>";
-    echo "</thead>";
-    echo "<tbody>";
-    while ($fila = $response->fetch_assoc())
+    if ($response = $class->query("SELECT $class->profesores.ID, $class->profesores.Nombre, $class->profesores.Iniciales, $class->profesores.Activo FROM $class->profesores WHERE $class->profesores[ID]='$_GET[ID]'")
     {
-        echo "<tr id='profesor_$fila[ID]' class='row_prof'>";
-        if($fila['Tipo'] == 'Admin')
+        if ($response->num_rows > 0)
+        {
+            echo "<div id='guardias'></div>";
+            echo "<h2>Edición de Guardias</h2>";
+            echo "</br><table id='tabla_profesores' class='table table-hover'>";
+            echo "<thead>";
+                echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>Nombre</th>";
+                    echo "<th>Iniciales</th>";
+                    echo "<th>Activo</th>";
+                echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while ($fila = $response->fetch_assoc())
             {
-              echo "<td>$fila[ID]</td>";
-              echo "<td>$fila[Nombre]</td>";
-              echo "<td>$fila[Iniciales]</td>";
-              echo "<td>$activo</td>";
-            }
-    }
-   }
-   else
-   {
-       $ERR_MSG = "Solo es una prueba";
-   }
- }
- else
- {
-    $ERR_MSG = "Solo es una prueba";
+                echo "<tr id='guardias_$fila[ID]' class='row_prof'>";
+                if($fila['Tipo'] == 'Admin')
+                    {
+                    echo "<td>$fila[ID]</td>";
+                    echo "<td>$fila[Nombre]</td>";
+                    echo "<td>$fila[Iniciales]</td>";
+                    echo "<td>$activo</td>";
+                    }
+                    else
+                    {
+                        $ERR_MSG = "Solo es una prueba";
 
- }
+                    }
+            }
+        }
+        else
+        {
+            $ERR_MSG = "Solo es una prueba";
+        }
+    }
+    else
+    {
+        $ERR_MSG = "Solo es una prueba";
+
+    }
 }
 else
 {
